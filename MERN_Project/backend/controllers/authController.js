8cc102101f1054d5abab3c7a56283622d75b6443
Saurabh-Borgaonkar <saurabhborgaonkar55@gmail.com>
2026-07-30 16:53:25 +0530

    registation Api succefully completed

--- MERN_Project/backend/controllers/authController.js
+++ MERN_Project/backend/controllers/authController.js
@@ -1,16 +1,19 @@
 const User=require("../models/User");
+const bycryptjs=require("bcryptjs");
 
 const register =async (req, res) => {
     const {name,email,password,role}=req.body;
-    console.log(req.body);
+    // console.log(req.body);
+
+    //for user side requirements
     if(!name || !email || !password){
-        return res.status(400).json({
+        return res.status(400).json({       //bad request 400
             success:false,
              message: "Please fill all required fields"
         });
     }
+    //we check the user is alredy exist 
     const isexitUser=await User.findOne({email});
-
     if(isexitUser){
         return res.status(409).json({
                  success: false,
@@ -18,11 +21,27 @@ const register =async (req, res) => {
       
         });
     }
-     res.json({
-        success: true,
-        message: "User not found, continue registration"
-    });
-    res.send("Data recevied");
+//pass hashing insted of direct password sroting we use bycryptjs
+      const hashedPwd=await bycryptjs.hash(password,10);
+    // console.log(hashedPwd);
+    const user=await User.create({
+        name,
+        email,
+        password:hashedPwd,
+        role
+    })
+
+    //201 define succefully work done
+    return res.status(201).json({
+    success: true,
+    message: "User registered successfully",
+    user
+});
 };
+
+const login=async (req,res)=>{
+    const {name,password}=req.body;
+    
+}
 
 module.exports = { register };
